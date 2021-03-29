5311f658c26df3d42f44f1916d128858281b46e1
VladFomich <vladfomich123@gmail.com>
2021-03-29 14:35:16 +0300

    Added 7 home work

---
+++ HomewWork_7/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    CharacterController controller;
+    public float speed = 12f;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    public float jumpHight = 3f;
+
+    private AudioSource audioSour;
+    public AudioClip clip;
+   
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        audioSour = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = Mathf.Sqrt(jumpHight * -2 * gravity);
+            }
+
+        }
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * h + transform.forward * v;
+        controller.Move(move * speed * Time.deltaTime);
+
+
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if ((move.z > 0 && move.x > 0) || !audioSour.isPlaying)
+        {
+            audioSour.Play();
+        }
+
+        if (move.z == 0 && move.x == 0)
+        {
+            audioSour.Stop();
+
+        }
+    }
+}
